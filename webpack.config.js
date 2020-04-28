f3c306bcf095c11908cc57eb0bc09d77328320ee
Zach Maynard <maynard.zach23@gmail.com>
2020-04-28 11:25:36 -0400

    webpack/darkmode

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,43 @@
 const path = require('path');
 
 module.exports = {
-  entry: './App.jsx',
+  entry: './src/App.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtools: 'inline-source-map',
+  devtool: 'inline-source-map',
   module: {
     rules: [
         {
-            test: /\.tsx$/,
-            use: 'ts-loader',
+            test: /\.(tsx|jsx)$/,
+            use: [
+                {
+                    loader: 'ts-loader',
+                    loader: 'babel-loader'
+                }
+            ],
             exclude: /node_modules/
-        }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          {
+            enforce: "pre",
+            test: /\.js$/,
+            loader: "source-map-loader"
+          }
     ]
   },
+  externals: {
+    "styled-components": {
+        commonjs: "styled-components",
+        commonjs2: "styled-components",
+        amd: "styled-components",
+    },
+  },
   resolve: {
-      extensions: ['.tsx', 'jsx', '.js']
+      extensions: ['.tsx', '.ts', 'jsx', '.js']
   }
 };
